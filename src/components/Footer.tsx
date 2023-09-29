142a5ff43f8663f21d44de52156e20c16779ad98
samtmorgan <sam.morgan@beacon-tech.co.uk>
2023-09-29 09:19:55 +0100

    refactor: remove stale code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/system';
-import { Link, Typography } from '@mui/material';
+import { Link } from '@mui/material';
 import styles from './style/footer.module.css';
 
 const privacyPolicyHref = `https://samtmorgan.github.io/turbo-telegram/privacy_policy.pdf`;
@@ -8,7 +8,6 @@ const privacyPolicyHref = `https://samtmorgan.github.io/turbo-telegram/privacy_p
 export function Footer() {
 	return (
 		<Box className={styles.footer}>
-			<Typography variant="body2">© 2023 Sam T Morgan</Typography>
 			<Link
 				target="_blank"
 				rel="noopener noreferrer"
