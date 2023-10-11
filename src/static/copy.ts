f3ca334843f15258f73131a8761ac2a51658e84f
samtmorgan <sam.morgan@beacon-tech.co.uk>
2023-10-11 10:27:52 +0100

    feat: vercel analytics

--- src/static/copy.ts
+++ src/static/copy.ts
@@ -1,12 +1,16 @@
-import { Titles } from '@/types/types';
+import { MetaGeneric } from '@/types/types';
 
-export const titles: Titles<string> = {
-  root: 'Sam T Morgan | Full Stack Developer',
-  home: 'Sam T Morgan | Home',
-  about: 'Sam T Morgan | About',
-  contact: 'Sam T Morgan | Contact',
+export const metaTitles: MetaGeneric<string> = {
+  root: 'Sam Trindade Morgan | Full Stack Developer',
+  home: 'Sam Trindade Morgan | Home',
+  about: 'Sam Trindade Morgan | About',
+  contact: 'Sam Trindade Morgan | Contact',
 };
-export const metaDescription =
-  'Sam T Morgan is a Full Stack Developer based in London, UK. He specialises in React and Next.js.';
+export const metaDescriptions: MetaGeneric<string> = {
+  root: 'Sam Trindade Morgan home page.',
+  about: 'About Sam Trindade Morgan | Full Stack Developer based in London, UK. Specialises in React and Next.js.',
+  contact: 'Contact Sam Trindade Morgan | Full Stack Developer based in London, UK. Specialises in React and Next.js.',
+};
+
 // 'Sam T Morgan, Full Stack Developer Portfolio, Front End Developer, React, Next',
 export const metaKeywords = 'portfolio, developer, full stack, front end, react, next, web';
